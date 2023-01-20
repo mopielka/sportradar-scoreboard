0fefbb17e62dd3a4323dd0fcb8ac944b098e282e
Michal Opielka <misiu9091909@gmail.com>
2023-01-20 13:09:22 +0100

    fix: regenerate dist

--- dist/scoreboard.d.ts
+++ dist/scoreboard.d.ts
@@ -1,6 +1,9 @@
 import type { Scoreboard } from './types';
 export declare const createScoreboard: () => Scoreboard;
-export declare const startGame: (scoreboard: Scoreboard, homeTeamName: string, awayTeamName: string) => Scoreboard;
+export declare const startGame: (scoreboard: Scoreboard, homeTeamName: string, awayTeamName: string) => {
+    newScoreboard: Scoreboard;
+    gameId: string;
+};
 export declare const finishGame: (scoreboard: Scoreboard, gameId: string) => Scoreboard;
 export declare const updateScore: (scoreboard: Scoreboard, gameId: string, homeScore: number, awayScore: number) => Scoreboard;
 //# sourceMappingURL=scoreboard.d.ts.map
--- dist/scoreboard.js
+++ dist/scoreboard.js
@@ -1,6 +1,9 @@
 import { createGame, updateGameScore } from './game';
 export const createScoreboard = () => [];
-export const startGame = (scoreboard, homeTeamName, awayTeamName) => [...scoreboard, createGame(homeTeamName, awayTeamName)];
+export const startGame = (scoreboard, homeTeamName, awayTeamName) => {
+    const game = createGame(homeTeamName, awayTeamName);
+    return { newScoreboard: [...scoreboard, game], gameId: game.id };
+};
 export const finishGame = (scoreboard, gameId) => scoreboard.filter((game) => game.id !== gameId);
 export const updateScore = (scoreboard, gameId, homeScore, awayScore) => scoreboard.map((game) => game.id === gameId ? updateGameScore(game, homeScore, awayScore) : game);
 //# sourceMappingURL=scoreboard.js.map
